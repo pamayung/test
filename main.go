a1eaed457950c51eeedc33124d4f10e617591f9d
pamayung <wildhan11@gmail.com>
2022-01-31 15:25:21 +0700

    init

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+
+}
+
+func hello(c echo.Context) error {
+	return c.JSON(http.StatusOK, "hello")
+}
